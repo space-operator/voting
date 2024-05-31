0aa54d60a3e9ea86f10064d95a980d00537370a1
enzotar <enzotar3000@gmail.com>
2024-05-30 23:58:47 -0700

    bn issues, invalid character, not a function...

--- src/components/voting/MultiChoiceVotes.tsx
+++ src/components/voting/MultiChoiceVotes.tsx
@@ -1,12 +1,12 @@
-import { Proposal, ProposalState } from "@solana/spl-governance";
+import { Proposal, ProposalState } from '@solana/spl-governance';
 
-import { BN } from "@coral-xyz/anchor";
+import { BN } from '@coral-xyz/anchor';
 
-import BigNumber from "bignumber.js";
-import { useRealmFromParams } from "@/app/api/realm/hooks";
-import { fmtBnMintDecimals } from "@/utils/units";
-import { CheckCircleIcon, ChevronRight } from "lucide-react";
-import { useMintInfo } from "@/app/api/token/hooks";
+import BigNumber from 'bignumber.js';
+import { useRealmFromParams } from '@/app/api/realm/hooks';
+import { fmtBnMintDecimals } from '@/utils/units';
+import { CheckCircleIcon, ChevronRight } from 'lucide-react';
+import { useMintInfo } from '@/app/api/token/hooks';
 
 export const MultiChoiceVotes = ({
   proposal,
@@ -26,7 +26,7 @@ export const MultiChoiceVotes = ({
       : councilMint;
 
   const totalVoteWeight = proposal.options.reduce(
-    (a, b) => new BN(a).add(new BN(b.voteWeight)),
+    (a, b) => new BN(a).add(new BN(b.voteWeight, 'hex')),
     new BN(0)
   );
 
@@ -35,56 +35,56 @@ export const MultiChoiceVotes = ({
   let highestWeight = new BN(0);
 
   for (const option of proposal.options) {
-    highestWeight = new BN(option.voteWeight).gt(highestWeight)
+    highestWeight = new BN(option.voteWeight, 'hex').gt(highestWeight)
       ? option.voteWeight
       : highestWeight;
   }
 
-  const nota = "$$_NOTA_$$";
+  const nota = '$$_NOTA_$$';
   const last = proposal.options.length - 1;
 
   return (
-    <div className="pb-4 px-6 border rounded-md">
+    <div className='pb-4 px-6 border rounded-md'>
       {proposal.options.slice(0, limit).map((option, index) => {
         const optionVotes = option.voteWeight;
         const optionWeightPct = totalVoteWeight.isZero() // dont divide by zero
           ? new BN(0)
-          : optionVotes.muln(1000).div(totalVoteWeight);
+          : new BN(optionVotes, 'hex').muln(1000).div(totalVoteWeight);
 
         return (
-          <div className="border-b border p-5" key={index}>
-            <div className="flex flex-row justify-between gap-2">
-              <div className="flex flex-row justify-start">
+          <div className='border-b border p-5' key={index}>
+            <div className='flex flex-row justify-between gap-2'>
+              <div className='flex flex-row justify-start'>
                 <div>
                   {option.label === nota && index === last
-                    ? "None of the Above"
+                    ? 'None of the Above'
                     : option.label}
                 </div>
                 {proposalMint === undefined ? null : (
                   <div>
                     {new BigNumber(optionVotes.toString())
                       .shiftedBy(-proposalMint.decimals)
-                      .toFormat(0)}{" "}
+                      .toFormat(0)}{' '}
                     votes
                   </div>
                 )}
               </div>
-              <div className="text-sm">
+              <div className='text-sm'>
                 {isComplete &&
                   !highestWeight.eq(new BN(0)) &&
                   option.voteWeight.eq(highestWeight) && (
-                    <CheckCircleIcon className="inline w-4 mr-1" />
+                    <CheckCircleIcon className='inline w-4 mr-1' />
                   )}
                 {fmtBnMintDecimals(optionWeightPct, 1)}%
               </div>
             </div>
-            <div className="bg-bkg-4 h-1 flex flex-grow mt-1.5 rounded w-full">
+            <div className='bg-bkg-4 h-1 flex flex-grow mt-1.5 rounded w-full'>
               <div
                 style={{
                   width: `${optionWeightPct.divn(10)}%`,
                 }}
                 className={`bg-primary-light flex rounded-l ${
-                  0 < 0.01 && "rounded"
+                  0 < 0.01 && 'rounded'
                 }`}
               ></div>
             </div>
@@ -92,11 +92,11 @@ export const MultiChoiceVotes = ({
         );
       })}
       {limit < proposal.options.length && (
-        <div className="border border-fgd-4 rounded-lg p-4">
-          <div className="flex flex-row gap-2">
-            <div className="">
+        <div className='border border-fgd-4 rounded-lg p-4'>
+          <div className='flex flex-row gap-2'>
+            <div className=''>
               {proposal.options.length - limit} more choice
-              {proposal.options.length - limit !== 1 && "s"}{" "}
+              {proposal.options.length - limit !== 1 && 's'}{' '}
             </div>
             <ChevronRight />
           </div>
