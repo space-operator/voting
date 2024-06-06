23d58a2cd049ae7ab023da3c8f7d63866512f6d7
enzotar <enzotar3000@gmail.com>
2024-06-06 10:12:01 -0700

    .

--- src/components/refresh.tsx
+++ src/components/refresh.tsx
@@ -3,12 +3,25 @@
 import { RefreshCwIcon } from 'lucide-react';
 import { Button } from './ui/button';
 import { revalidateProposals } from '@/app/api/proposals/actions';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export const Refresh = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleRefresh = () => {
+    setIsLoading(true);
+    revalidateProposals().then(() => {
+      router.refresh();
+      setIsLoading(false);
+    });
+  };
+
   return (
-    <Button variant='outline' className='border-primary'>
+    <Button variant='outline' className='border-primary' disabled={isLoading}>
       <RefreshCwIcon
-        onClick={() => revalidateProposals()}
+        onClick={handleRefresh}
         className='cursor-pointer'
       />
     </Button>
